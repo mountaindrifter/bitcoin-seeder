19695afc84bb2b9818af4987d997167801ec7da6
mountaindrifter <s.c@rman.email>
2017-05-20 11:55:17 -0600

    update zencash magic start value

--- protocol.cpp
+++ protocol.cpp
@@ -22,7 +22,7 @@ static const char* ppszTypeName[] =
     "block",
 };
 
-unsigned char pchMessageStart[4] = { 0xf9, 0xbe, 0xb4, 0xd9 };
+unsigned char pchMessageStart[4] = { 0x63, 0x61, 0x73, 0x6 };
 
 CMessageHeader::CMessageHeader()
 {
